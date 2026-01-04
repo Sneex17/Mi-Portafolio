baa2419b72bb54009975e4321bd32af20f6cde3b
Sneex17 <228738464+Sneex17@users.noreply.github.com>
2026-01-04 13:46:58 -0400

    Header responsive for tablets and cellphones

---
+++ Aps/nav.js
@@ -0,0 +1,15 @@
+const NavBtun = document.querySelector(".boton");
+const NavCont = document.querySelector(".content-nav");
+
+NavBtun.addEventListener("click", () => {
+NavCont.classList.toggle("nav-menu-v")
+
+})
+
+
+/*const LinkBtun = document.querySelector(".nav-link");
+
+LinkBtun.addEventListener("click", () => {
+NavCont.classList.remove("nav-menu-v")
+
+})*/
